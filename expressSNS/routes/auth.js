bdb65eda55b6b7d211857bd73e5890b52c27a484
Nicole <pumpkinyeah3@gmail.com>
2023-08-18 19:06:49 +0900

    kakao logout

--- expressSNS/routes/auth.js
+++ expressSNS/routes/auth.js
@@ -13,7 +13,7 @@ router.post('/join', isNotLoggedIn, join);
 router.post('/login', isNotLoggedIn, login);
 
 // GET /auth/logout
-router.get('/logout', isLoggedIn, logout);
+router.get('/logoutlocal', isLoggedIn, logout);
 
 // GET / auth/kakao
 router.get('/kakao', passport.authenticate('kakao'));
@@ -23,6 +23,17 @@ router.get('/kakao/callback', passport.authenticate('kakao', {
     failureRedirect: '/?error=카카오로그인 실패',
 }), (req, res) => {
     res.redirect('/');  // 성공 시에는 /로 이동
+});
+
+router.get('/logoutkakao', isLoggedIn, async (req, res, next) => {
+    const redirectLogoutURI = "https://kauth.kakao.com/oauth/logout?client_id=" + process.env.KAKAO_ID + "&logout_redirect_uri=http://localhost:8001/auth/kakao/logout"
+    res.redirect(redirectLogoutURI);
+});
+
+router.get('/kakao/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/');
+    });
 });
 
 module.exports = router;
